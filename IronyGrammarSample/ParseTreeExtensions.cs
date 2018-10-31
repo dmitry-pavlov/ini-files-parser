00b141d95a4d41221abc47e969f8022865b00f4e
Alexey V. Ryazhskikh <musukvl@gmail.com>
2018-10-31 16:52:06 +0300

    initial commit

---
+++ IronyGrammarSample/ParseTreeExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using Irony.Parsing;
+
+namespace IronyGrammarSample
+{
+    public static class ParseTreeExtensions
+    {
+        public static string ToXml(this ParseTree parseTree)
+        {
+            if (parseTree == null || parseTree.Root == null)
+                return string.Empty;
+            XmlDocument xmlDocument = parseTree.ToXmlDocument();
+            StringWriter stringWriter = new StringWriter();
+            XmlTextWriter xmlTextWriter1 = new XmlTextWriter((TextWriter)stringWriter);
+            xmlTextWriter1.Formatting = Formatting.Indented;
+            XmlTextWriter xmlTextWriter2 = xmlTextWriter1;
+            xmlDocument.WriteTo((XmlWriter)xmlTextWriter2);
+            xmlTextWriter1.Flush();
+            return stringWriter.ToString();
+        }
+
+        public static XmlDocument ToXmlDocument(this ParseTree parseTree)
+        {
+            XmlDocument ownerDocument = new XmlDocument();
+            if (parseTree == null || parseTree.Root == null)
+                return ownerDocument;
+            XmlElement element = ownerDocument.CreateElement("ParseTree");
+            ownerDocument.AppendChild((XmlNode)element);
+            XmlElement xmlElement = parseTree.Root.ToXmlElement(ownerDocument);
+            element.AppendChild((XmlNode)xmlElement);
+            return ownerDocument;
+        }
+
+        public static XmlElement ToXmlElement(this ParseTreeNode node, XmlDocument ownerDocument)
+        {
+            XmlElement element = ownerDocument.CreateElement("Node");
+            element.SetAttribute("Term", node.Term.Name);
+            BnfTerm term = node.Term;
+            if (term.HasAstConfig() && term.AstConfig.NodeType != (Type)null)
+                element.SetAttribute("AstNodeType", term.AstConfig.NodeType.Name);
+            if (node.Token != null)
+            {
+                element.SetAttribute("Terminal", node.Term.GetType().Name);
+                if (node.Token.Value != null)
+                    element.SetAttribute("Value", node.Token.Value.ToString());
+            }
+            else
+            {
+                foreach (ParseTreeNode childNode in (List<ParseTreeNode>)node.ChildNodes)
+                {
+                    XmlElement xmlElement = childNode.ToXmlElement(ownerDocument);
+                    element.AppendChild((XmlNode)xmlElement);
+                }
+            }
+            return element;
+        }
+    }
+}
---
+++ IronyGrammarSample/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq; 
+using Irony.Parsing;
+
+namespace IronyGrammarSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var expressionGrammar = new MyLanguageGrammar();
+            var parser = new Parser(expressionGrammar);
+         
+            ParseQuery(@"
+[Option1]
+woof: 123
+bubu: 3211 
+
+[Option2]
+test: 321
+", parser);
+
+            ParseQuery(@"
+woof: 123
+bubu: 3211 
+
+", parser);
+
+
+            Console.WriteLine("Done");
+        }
+
+
+        private static ParseTree ParseQuery(string query, Parser exparser)
+        {
+            var parseResult = exparser.Parse(query);
+
+ 
+            Console.WriteLine(query);
+
+            if (parseResult.HasErrors())
+            {
+                foreach (var s in parseResult.ParserMessages.Select(x => x.Message))
+                {
+                    Console.Write(s);
+                }
+                return null;
+            } 
+            Console.WriteLine(parseResult.ToXml());
+            return parseResult;
+        }
+    }
+
+    internal class MyLanguageGrammar : Grammar
+    {
+        public MyLanguageGrammar()
+        {
+            var optionIndentifier = new IdentifierTerminal("OptionIdentifier"); 
+
+            var optionTerm = new NonTerminal("Option");
+            optionTerm.Rule = ToTerm("[") + optionIndentifier + "]";
+            
+            var numericFildValue = new NumberLiteral("NumericFieldValue");
+            var fieldValue = new NonTerminal("fieldValue");
+            fieldValue.Rule = numericFildValue;
+            MarkTransient(fieldValue);
+
+
+            var field = new NonTerminal("Field");
+            var fieldName = new IdentifierTerminal("fieldName");
+
+            field.Rule = fieldName + ":" + fieldValue;
+
+            var blockFields = new NonTerminal("BlockFields");        
+            MakePlusRule(blockFields, field);
+
+            var optionBlock = new NonTerminal("Option Block");
+
+            optionBlock.Rule = optionTerm + blockFields;
+
+            var blockList = new NonTerminal("BlockList");
+            MakePlusRule(blockList, optionBlock);
+
+            var configRoot = new NonTerminal("ConfigRoot");
+            configRoot.Rule = blockList | blockFields;
+            this.Root = configRoot;
+        }
+    }
+}
